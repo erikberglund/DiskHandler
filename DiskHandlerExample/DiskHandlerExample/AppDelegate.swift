9cd842ffb58eb861a8c31fc5de8501553196e670
Erik Berglund <erikberglund@Eriks-MacBook-Pro.local>
2017-06-24 08:52:37 +0200

    Initial Commit

---
+++ DiskHandlerExample/DiskHandlerExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DiskHandlerExample
+//
+//  Created by Erik Berglund on 2017-06-24.
+//  Copyright © 2017 Erik Berglund. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
