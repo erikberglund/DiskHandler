ba9a161776753a0fbb8074fcf9f8f5815d0eb753
Erik Berglund <erikberglund@Eriks-MacBook-Pro.local>
2017-06-24 08:43:39 +0200

    Initial Commit

---
+++ DiskHandler/DiskHandler/DiskHandler.h
@@ -0,0 +1,19 @@
+//
+//  DiskHandler.h
+//  DiskHandler
+//
+//  Created by Erik Berglund on 2017-06-24.
+//  Copyright © 2017 Erik Berglund. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DiskHandler.
+FOUNDATION_EXPORT double DiskHandlerVersionNumber;
+
+//! Project version string for DiskHandler.
+FOUNDATION_EXPORT const unsigned char DiskHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiskHandler/PublicHeader.h>
+
+
